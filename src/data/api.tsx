2a11aa9ffb7e123eb946c08d2e1fdcff05bb0521
justjo3l <comedyandcomputers@gmail.com>
2025-03-22 19:24:32 +1100

    Add workflow changes for dev and prod

--- src/data/api.tsx
+++ src/data/api.tsx
@@ -1,4 +1,6 @@
-export const collectionEndpoint = "https://2vyhl58mci.execute-api.ap-southeast-2.amazonaws.com/1/get-upload-url";
-export const collectionBucket = "sierra-e-bucket";
-export const retrievalCSVEndpoint = "https://tni794cvna.execute-api.ap-southeast-2.amazonaws.com/1/get-csv-data";
-export const retrievalJSONEndpoint = "https://tni794cvna.execute-api.ap-southeast-2.amazonaws.com/1/get-json-data";
+const ENV = process.env.REACT_APP_ENV || "dev"; // Default to "prod" if not set
+
+export const collectionEndpoint = `https://2vyhl58mci.execute-api.ap-southeast-2.amazonaws.com/${ENV}/get-upload-url`;
+export const collectionBucket = ENV === "dev" ? "dev-sierra-e-bucket" : "sierra-e-bucket";
+export const retrievalCSVEndpoint = `https://tni794cvna.execute-api.ap-southeast-2.amazonaws.com/${ENV}/get-csv-data`;
+export const retrievalJSONEndpoint = `https://tni794cvna.execute-api.ap-southeast-2.amazonaws.com/${ENV}/get-json-data`;
