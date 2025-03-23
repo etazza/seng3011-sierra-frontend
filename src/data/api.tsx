f03516979a4439eecb5cd514ac6a8433777b16cf
justjo3l <comedyandcomputers@gmail.com>
2025-03-23 15:09:06 +1100

    Update to Vite configuration

--- src/data/api.tsx
+++ src/data/api.tsx
@@ -1,4 +1,4 @@
-const ENV = process.env.REACT_APP_ENV || "dev"; // Default to "dev" if not set
+const ENV = import.meta.env.VITE_ENV || "dev"; // Default to "dev" if not set
 
 export const collectionEndpoint = `https://2vyhl58mci.execute-api.ap-southeast-2.amazonaws.com/${ENV}/get-upload-url`;
 export const collectionBucket = ENV === "dev" ? "dev-sierra-e-bucket" : "sierra-e-bucket";
