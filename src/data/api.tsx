9e78922838a59fba36e0ddb185bd0fa305fbc737
justjo3l <comedyandcomputers@gmail.com>
2025-04-02 04:41:09 +1100

    Change frontend url

--- src/data/api.tsx
+++ src/data/api.tsx
@@ -6,6 +6,6 @@ export const clientSecret = import.meta.env.VITE_CLIENT_SECRET || "";
 
 export const collectionEndpoint = `https://2vyhl58mci.execute-api.ap-southeast-2.amazonaws.com/${ENV}/get-upload-url`;
 export const collectionBucket = ENV === "dev" ? "dev-sierra-e-bucket" : "sierra-e-bucket";
-export const frontend = ENV === "dev" ? "https://d3kuyfddew0um3.cloudfront.net%2Fupload" : "https://sierra-frontend.s3-website-ap-southeast-2.amazonaws.com";
+export const frontend = ENV === "dev" ? "https://d3kuyfddew0um3.cloudfront.net/upload" : "https://sierra-frontend.s3-website-ap-southeast-2.amazonaws.com";
 export const retrievalCSVEndpoint = `https://tni794cvna.execute-api.ap-southeast-2.amazonaws.com/${ENV}/get-csv-data`;
 export const retrievalJSONEndpoint = `https://tni794cvna.execute-api.ap-southeast-2.amazonaws.com/${ENV}/get-json-data`;
