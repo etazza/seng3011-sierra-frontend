04bf10c091594daa952222d36c39af4523f263d4
justjo3l <comedyandcomputers@gmail.com>
2025-04-02 16:19:00 +1100

    Fix header order

--- src/components/DataUpload.tsx
+++ src/components/DataUpload.tsx
@@ -49,8 +49,8 @@ export function DataUpload() {
     console.log("Uploading file: " + file.name);
     const xhr = new XMLHttpRequest();
     const urlWithParams = `${collectionEndpoint}?file=${file.name}`;
-    xhr.setRequestHeader("Authorization", `Bearer ${user.token}`);
     xhr.open('GET', urlWithParams);
+    xhr.setRequestHeader("Authorization", `Bearer ${user.token}`);
     xhr.onload = function() {
       const res = JSON.parse(xhr.response);
       if (xhr.status === 200 && res["URL"]) {
